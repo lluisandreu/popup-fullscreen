3e9a957b4599bf92b9be6613f48e149f8e4afec3
Lluis Oliver <oliverobrador@gmail.com>
2014-08-22 13:58:14 +0100

    Important changes, entypo and fallback for ie7 and 1e8

--- popupnav.js
+++ popupnav.js
@@ -1,20 +1,17 @@
 (function($){
     jQuery(document).ready(function($) {
         var menuTrigger = $('.popupnav-trigger');
-        var menuCloser = $('.popupnav-close a');
         menuTrigger.on('click',  function(event) {
             event.preventDefault();
-            $('#popupnav-fullscreen').addClass('visible');
-        });
-        menuCloser.on('click', function(event) {
-            event.preventDefault();
-            $('#popupnav-fullscreen').removeClass('visible');
+            $('#popupnav-fullscreen').removeClass('closed').addClass('visible');
+            $('html').css('overflow', 'hidden');
         });
         $('#popupnav-fullscreen a').on('click', function(event) {
             event.preventDefault();
             var location = $(this).attr('href');
             window.location = location;
-            $('#popupnav-fullscreen').removeClass('visible');
+            $('#popupnav-fullscreen').removeClass('visible').addClass('closed');
+            $('html').css('overflow', 'visible');
         });
     });
 })(jQuery);
