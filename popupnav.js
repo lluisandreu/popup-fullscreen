1cbe7d06b735f2e715dce7dfcb8924968e831556
Lluis Oliver <oliverobrador@gmail.com>
2014-09-15 15:51:41 +0100

    Added responsive settings

--- popupnav.js
+++ popupnav.js
@@ -1,18 +1,38 @@
-(function($){
-    jQuery(document).ready(function($) {
-        var menuTrigger = $('.popupnav-trigger');
-        menuTrigger.on('click',  function(event) {
-            event.preventDefault();
-            $('#popupnav-fullscreen').removeClass('closed').addClass('visible');
-            $('html').css('overflow', 'hidden');
-        });
-        // Clicking any link will close the overlay
-        $('#popupnav-fullscreen a').on('click', function(event) {
-            event.preventDefault();
-            var location = $(this).attr('href');
-            window.location = location;
-            $('#popupnav-fullscreen').removeClass('visible').addClass('closed');
-            $('html').css('overflow', 'visible');
-        });
+(function($) {
+    var menuTrigger = $('.popupnav-trigger');
+    var breakpoint = $('#popupnav-fullscreen').data('breakpoint');
+    var blockId = $('#block-popupnav-popupnav');
+    var replaceMenuId = $('#popupnav-fullscreen').data('replace');
+    var replaceMenu = null;
+    if(replaceMenuId.indexOf('#') !== -1 && replaceMenuId.charAt(0) === '#'){
+       replaceMenu = replaceMenuId;
+    }
+
+    $(window).on('resize load',  function(event) {
+        var winSize = document.documentElement.clientWidth || document.body.clientWidth;
+        if (winSize >= breakpoint) {
+            blockId.hide();
+            if(replaceMenu != null){
+               $(replaceMenu).show();
+            }
+        } else {
+            blockId.show();
+            if(replaceMenu != null){
+                $(replaceMenu).hide();
+            }
+        }
+    });
+    menuTrigger.on('click', function(event) {
+        event.preventDefault();
+        $('#popupnav-fullscreen').removeClass('closed').addClass('visible');
+        $('html').css('overflow', 'hidden');
+    });
+    // Clicking any link will close the overlay
+    $('#popupnav-fullscreen a').on('click', function(event) {
+        event.preventDefault();
+        var location = $(this).attr('href');
+        window.location = location;
+        $('#popupnav-fullscreen').removeClass('visible').addClass('closed');
+        $('html').css('overflow', 'visible');
     });
 })(jQuery);
