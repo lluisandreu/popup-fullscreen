fcab67b0254ab83c70cc9b701f1ff9a81aba05cb
Lluis Oliver <oliverobrador@gmail.com>
2014-09-19 13:46:29 +0100

    Added collapsible feature

--- popupnav.js
+++ popupnav.js
@@ -4,35 +4,89 @@
     var blockId = $('#block-popupnav-popupnav');
     var replaceMenuId = $('#popupnav-fullscreen').data('replace');
     var replaceMenu = null;
-    if(replaceMenuId.indexOf('#') !== -1 && replaceMenuId.charAt(0) === '#'){
-       replaceMenu = replaceMenuId;
+    if (replaceMenuId) {
+        if (replaceMenuId.indexOf('#') !== -1 && replaceMenuId.charAt(0) === '#') {
+            replaceMenu = replaceMenuId;
+        }
     }
 
-    $(window).on('resize load',  function(event) {
+    $(window).on('load resize', function(event) {
         var winSize = document.documentElement.clientWidth || document.body.clientWidth;
-        if (winSize >= breakpoint) {
-            blockId.hide();
-            if(replaceMenu != null){
-               $(replaceMenu).show();
+        if (breakpoint) {
+            if (winSize >= breakpoint) {
+                blockId.hide();
+                if (replaceMenu != null) {
+                    $(replaceMenu).show();
+                }
+            } else {
+                blockId.show();
+                if (replaceMenu != null) {
+                    $(replaceMenu).hide();
+                }
             }
         } else {
             blockId.show();
-            if(replaceMenu != null){
-                $(replaceMenu).hide();
-            }
         }
     });
+
+    if($('nav.popupnav-navigation').hasClass('collapse-children')){
+            collapseChildren();
+    }
+
     menuTrigger.on('click', function(event) {
         event.preventDefault();
-        $('#popupnav-fullscreen').removeClass('closed').addClass('visible');
-        $('html').css('overflow', 'hidden');
+        openMenu();
+
     });
-    // Clicking any link will close the overlay
+
     $('#popupnav-fullscreen a').on('click', function(event) {
         event.preventDefault();
-        var location = $(this).attr('href');
-        window.location = location;
-        $('#popupnav-fullscreen').removeClass('visible').addClass('closed');
-        $('html').css('overflow', 'visible');
+        closeMenu();
+        goTo($(this));
     });
+
+    $('html').live('keydown', function(e) {
+        if (e.keyCode == 27) {
+            e.preventDefault();
+            closeMenu();
+        }
+    });
+
+    function collapseChildren(){
+        var parent = $('li.parent-item');
+        var trigger = '<span class="collapse-trigger collapsed">&#59236;</span>';
+        parent.append(trigger);
+        $('.collapse-trigger').click(function(event) {
+            if($(this).hasClass('collapsed')){
+                openCollapse($(this));
+            } else {
+                collapse($(this));
+            }
+        });
+    }
+    function openCollapse(element) {
+        element.siblings('ul.collapse').slideDown(400);
+        element.html('&#59239;').removeClass('collapsed').addClass('open');
+    }
+
+    function collapse(element){
+        element.siblings('ul.collapse').slideUp(400);
+        element.html('&#59236;').removeClass('open').addClass('collapsed');
+    }
+
+    function openMenu() {
+        $('#popupnav-fullscreen').removeClass('closed').addClass('visible');
+        $('html, body').css('overflow', 'hidden');
+    }
+
+    function goTo(element){
+        var location = element.attr('href');
+        window.location = location;
+    }
+
+    function closeMenu() {
+        $('#popupnav-fullscreen').removeClass('visible').addClass('closed');
+        $('html, body').css('overflow', 'visible');
+    }
+
 })(jQuery);
